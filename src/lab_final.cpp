c193cbf06ca0b488b67a7f264e7a2783ec5c530f
Goro Yeh <jennyyeh56789@gmail.com>
2021-06-17 18:31:23 +0800

    62sec

--- src/lab_final.cpp
+++ src/lab_final.cpp
@@ -49,7 +49,7 @@ float error_theta;
 
 // goal = [[10,9],[8.7,11.7],[6,13],[3.3,11.7],[2,9],[3,7],[2,5],[3.3,2.3],[6,1],[8.7,2.3],[10,5],[9,7]]
 // Goal pose for robot. {x, y, theta}
-std::vector<std::vector<double>> GOALS={
+std::vector<std::vector<double>> GOALS12={
 {15.95,	15.555},
 {15.4365,	16.6215},
 {14.37,	17.135},
@@ -66,20 +66,20 @@ std::vector<std::vector<double>> GOALS={
 
 
 
-// std::vector<std::vector<double>> GOALS={
-// {15.95,	15.555}, {15.69325,16.08825},
-// {15.4365,	16.6215}, {14.90325,16.87825},
-// {14.37,	17.135}, {13.83675,16.87825},
-// {13.3035,	16.6215},{13.04675,16.08825},
-// {12.79,	15.555},{12.9875,	15.16},
-// {13.185,	14.765},{12.9875,	14.37},
-// {12.79,	13.975},{13.04675,13.44175},
-// {13.3035,	12.9085},{13.83675,12.65175},
-// {14.37,	12.395},{14.90325,	12.65175},
-// {15.4365,	12.9085}, {15.69325,	13.44175},
-// {15.95,	13.975},{15.7525,	14.37},
-// {15.555,	14.765},{15.7525,15.16}
-// };
+std::vector<std::vector<double>> GOALS24={
+{15.95,	15.555}, {15.69325,16.08825},
+{15.4365,	16.6215}, {14.90325,16.87825},
+{14.37,	17.135}, {13.83675,16.87825},
+{13.3035,	16.6215},{13.04675,16.08825},
+{12.79,	15.555},{12.9875,	15.16},
+{13.185,	14.765},{12.9875,	14.37},
+{12.79,	13.975},{13.04675,13.44175},
+{13.3035,	12.9085},{13.83675,12.65175},
+{14.37,	12.395},{14.90325,	12.65175},
+{15.4365,	12.9085}, {15.69325,	13.44175},
+{15.95,	13.975},{15.7525,	14.37},
+{15.555,	14.765},{15.7525,15.16}
+};
 
 bool start = false;
 
@@ -242,7 +242,6 @@ float max_v, max_w;
 // const float Kp = 0.5;
 float Kp, Ka, Kb, Krho;
 
-
 int counter = 0; // index for which goal we are trying to reach.
 
 float delta_x, delta_y, delta_theta, theta_r;
@@ -277,6 +276,9 @@ float deg2rad(float degree){
 }
 
 
+float distance = 0;
+int goals_mode;
+
 int main(int argc, char **argv)
 {
     // Initialize the node here
@@ -285,17 +287,52 @@ int main(int argc, char **argv)
 
     // Get parameters.
     float X_CRITERION, Y_CRITERION, THETA_CRITERION;
-    node.param("X_CRITERION", X_CRITERION, float(0.1));
-    node.param("Y_CRITERION", Y_CRITERION, float(0.1));
-    node.param("THETA_CRITERION", THETA_CRITERION, float(0.3));
-    node.param("Kp", Kp, float(0.6));
-    node.param("Krho", Krho, float(0.5));
-    node.param("Ka", Ka, float(2.5));
-    node.param("Kb", Kb, float(-1.12));
-    node.param("max_v", max_v, float(1.0));
-    node.param("max_w", max_w, float(1.0));
+    double DISTANCE_THRESHOLD;
+    // double goals_mode;
+    // node.param("X_CRITERION", X_CRITERION, float(0.1));
+    // node.param("Y_CRITERION", Y_CRITERION, float(0.1));
+    // node.param("THETA_CRITERION", THETA_CRITERION, float(0.3));
+    // node.param("Kp", Kp, float(0.6));
+    // node.param("Krho", Krho, float(0.5));
+    // node.param("Ka", Ka, float(2.5));
+    // node.param("Kb", Kb, float(-1.12));
+    // node.param("max_v", max_v, float(1.0));
+    // node.param("max_w", max_w, float(1.0));
+    // node.param("distance_threshold",DISTANCE_THRESHOLD, float(0.5));
+    // node.param("goals_mode", goals_mode, float(24.0));
+
+    ros::param::param<std::float_t>("/X_CRITERION", X_CRITERION, 0.1);
+    ros::param::param<std::float_t>("/Y_CRITERION", Y_CRITERION, 0.1);    
+    ros::param::param<std::float_t>("/THETA_CRITERION", THETA_CRITERION,0.35);
+    ros::param::param<std::float_t>("/Kp", Kp, 0.8); 
+    ros::param::param<std::float_t>("/Krho", Krho, 0.5);
+    ros::param::param<std::float_t>("/Ka", Ka, 2.5); 
+    ros::param::param<std::float_t>("/Kb", Kb, -1.12);
+    ros::param::param<std::float_t>("/max_v", max_v, 0.5); 
+    ros::param::param<std::float_t>("/max_w", max_w, 0.8); 
+    ros::param::param<std::double_t>("DISTANCE_THRESHOLD", DISTANCE_THRESHOLD, 0.8); 
+    ros::param::param<std::int32_t>("/goals_mode", goals_mode,  12 );
 
 
+
+    ROS_INFO("goals_mode: %d",goals_mode);
+
+    /* -- Initialize GOALS ---*/
+    std::vector<std::vector<double>> GOALS;
+    if( goals_mode==12) {
+        for(int i=0; i<GOALS12.size(); i++){
+                GOALS.push_back(GOALS12[i]);
+        }
+    }
+    else{
+        for(int i=0; i<GOALS24.size(); i++){
+                GOALS.push_back(GOALS24[i]);
+        }
+    }
+
+    ROS_INFO("Done init GOALS.\n");
+    for(auto &v: GOALS)
+        ROS_INFO("{ %.2f, %.2f}",v[0],v[1]);
 
 	ros::Publisher cmd_vel_pub = node.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
 	ros::Subscriber robot_pose_sub = node.subscribe("/status", 10, Robot_Pose_Callback);
@@ -303,7 +340,7 @@ int main(int argc, char **argv)
 
     ros::Subscriber Start_sub = node.subscribe("/start", 10, Start_Callback);
     // Set the publish rate here
-	ros::Rate rate(100);
+	ros::Rate rate(1000);
 
     float v, w;
 	geometry_msgs::Twist twist;
@@ -312,7 +349,7 @@ int main(int argc, char **argv)
     // Main Control Loop.
 
     // Measure Running Time
-    ros::WallTime start_, end_;
+    ros::WallTime start_, end_, now;
     bool start_measure = false;
     // After receive a new goal: keep localization task.
 	while (ros::ok()) 
@@ -324,16 +361,23 @@ int main(int argc, char **argv)
                 start_measure = true;
             }
 
-            if(counter >= 2*GOALS.size()){
-                break;
+            if(counter >= 2*GOALS.size()+1){
+                // Publish!
+                break; // End program.
             }
 
-                goal_x = GOALS[counter%GOALS.size()][0];
-                goal_y = GOALS[counter%GOALS.size()][1];
-                // goal_theta = GOALS[counter%GOALS.size()][2];
-                dx = GOALS[(counter+1)%GOALS.size()][0] - goal_x;
-                dy = GOALS[(counter+1)%GOALS.size()][1] - goal_y;
-                goal_theta = atan2(dy,dx);
+            goal_x = GOALS[counter%GOALS.size()][0];
+            goal_y = GOALS[counter%GOALS.size()][1];
+            // goal_theta = GOALS[counter%GOALS.size()][2];
+            dx = GOALS[(counter+1)%GOALS.size()][0] - goal_x;
+            dy = GOALS[(counter+1)%GOALS.size()][1] - goal_y;
+            goal_theta = atan2(dy,dx);
+
+            // goal_x = robot_x + 1;
+            // goal_y = robot_y + 1;
+            // dx = goal_x - robot_x;
+            // dy = goal_y - robot_y;
+            // goal_theta = atan2(dy,dx);
 
 
             goal_theta = thetaConstraint(goal_theta);
@@ -343,6 +387,8 @@ int main(int argc, char **argv)
             error_theta = goal_theta - robot_theta;
             error_theta = thetaConstraint(error_theta);
 
+            
+            distance = sqrt(pow(error_x, 2) + pow(error_y,2));
             // std::cout<<"Robot at: "<<robot_x << ", y: "<<robot_y <<", theta: "<< robot_theta <<", Goal: "<<goal_x<<" "<<goal_y<<" "<<goal_theta<< std::endl;
             // ROS_INFO("State: %d, Goal theta: %.2f, Robot Theta: %.2f", state, goal_theta, robot_theta);
             // ROS_INFO("Error: %.3f %.3f %.3f", error_x, error_y, error_theta);
@@ -354,48 +400,72 @@ int main(int argc, char **argv)
             {
             case MOVING:  
                 ////////// Main Control Loop! /////////////
-                if (abs(error_x) < X_CRITERION && abs(error_y) < Y_CRITERION && abs(error_theta) < THETA_CRITERION)
-                {
-                // if (abs(error_x) < 0.01 && abs(error_y) < 0.01)
-                // if (abs(error_x) < 0.3 && abs(error_y) < 0.3)
-                // {
-                    state = IDLE;
-                }
-                else if (abs(error_x) < X_CRITERION && abs(error_y) < Y_CRITERION )
-                {
-                    state = TURNING;
-                }
-                else
-                {
-                    /* ----- For Consistency ----- */
-                    delta_x = error_x;
-                    delta_y = error_y;
-                    delta_theta = -error_theta;
-                    theta_r = goal_theta;
-                
-                
-                    // cout<<"Moving now!!\n";
-                    rho = sqrt(pow(delta_x,2) + pow(delta_y,2));
-                    alpha = atan2(delta_y,delta_x) - theta_r - delta_theta;
-                    alpha = thetaConstraint(alpha);
-                    
-                    beta = -1*(alpha + delta_theta);
-                    beta = thetaConstraint(beta);
-                    
-                    v = Krho*rho;
-                    w = Ka*alpha + Kb*beta;
+                if(distance < DISTANCE_THRESHOLD && counter>0){
+                //     // state = TURNING;
+                    now = ros::WallTime::now();
+                    std::cout<<"Dis: "<<distance<<", Done running "<<counter<<", time executed: "<<(now - start_).toNSec()* 1e-9<<" sec"<<std::endl;
+                    counter++;                    
 
-                    /* --- Control Law --- */
-                    // v = Krho * rho;
-                    // w = Ka * alpha + Kb * beta;
-                    // ROS_INFO("Robot is MOVING !!!");
+                //     // counter++;
+                //     // std::cout<<"Next goal: "<<counter<<std::endl;
+                //     // break;
+                }
+                else{
+                    if (abs(error_x) < X_CRITERION && abs(error_y) < Y_CRITERION && abs(error_theta) < THETA_CRITERION)
+                    {
+                    // if (abs(error_x) < 0.01 && abs(error_y) < 0.01)
+                    // if (abs(error_x) < 0.3 && abs(error_y) < 0.3)
+                    // {
+                        now = ros::WallTime::now();
+                        std::cout<<"Else - Dis: "<<distance<<", Done running "<<counter<<", time executed: "<<(now - start_).toNSec()* 1e-9<<" sec"<<std::endl;
+                        counter++;  
+                        // state = IDLE;
+                    }
+                    // else if (abs(error_x) < X_CRITERION && abs(error_y) < Y_CRITERION )
+                    // {
+                    //     state = TURNING;
+                    // }
+                    else
+                    {   
+                        // std::cout<<"dis: "<<distance<<std::endl;
+                        /* ----- For Consistency ----- */
+                        delta_x = error_x;
+                        delta_y = error_y;
+                        delta_theta = -error_theta;
+                        theta_r = goal_theta;
+                    
+                    
+                        // cout<<"Moving now!!\n";
+                        rho = sqrt(pow(delta_x,2) + pow(delta_y,2));
+                        alpha = atan2(delta_y,delta_x) - theta_r - delta_theta;
+                        alpha = thetaConstraint(alpha);
+                        
+                        beta = -1*(alpha + delta_theta);
+                        beta = thetaConstraint(beta);
+                        
+                        v = Krho*rho;
+                        w = Ka*alpha + Kb*beta;
+
+                        /* --- Control Law --- */
+                        // v = Krho * rho;
+                        // w = Ka * alpha + Kb * beta;
+                        // ROS_INFO("Robot is MOVING !!!");
+                    }
                 }
                 break;
             case TURNING:
                 
                 if (abs(error_theta) < THETA_CRITERION || abs(error_theta + 2 * pi) < THETA_CRITERION || abs(error_theta - 2 * pi) < THETA_CRITERION)
                 {
-                    state = IDLE;
+                    // state = IDLE;
+                    state = MOVING;
+                    now = ros::WallTime::now();
+                    std::cout<<"Dis: "<<distance<<"Done running "<<counter<<", time executed: "<<(now - start_).toNSec()* 1e-9<<" sec"<<std::endl;
+                    counter++;                    
+
+                    // counter++;
+                    // std::cout<<"Next goal: "<<counter<<std::endl;
+                    break;                    
                 }
                 else
                 {
@@ -412,7 +482,8 @@ int main(int argc, char **argv)
                 w = 0;
                 // ROS_INFO("Robot IDLE and break!");
                 state = MOVING;
-                std::cout<<"Done running "<<counter<<std::endl;
+                now = ros::WallTime::now();
+                std::cout<<"Done running "<<counter<<", time executed: "<<(now - start_).toNSec()* 1e-9<<" sec"<<std::endl;
                 counter++;
         #ifdef DEBUG_LAB2
                 ROS_INFO("Robot reached the goal and is now IDLE.");
@@ -454,6 +525,16 @@ int main(int argc, char **argv)
         rate.sleep();
 
     }
+
+    // First STOP the robot
+    twist.linear.x =  0;
+    twist.linear.y = 0;
+    twist.linear.z = 0;
+    twist.angular.x = 0;
+    twist.angular.y = 0;
+    twist.angular.z = 0;
+    cmd_vel_pub.publish(twist); 
+
 
     // End running two laps.
     end_ = ros::WallTime::now();
